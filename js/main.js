8b8b85cab6f87593df5335326a05253f0b648006
DarioAversano <risusveloce@gmail.com>
2023-03-30 16:54:52 +0200

    commit html

--- js/main.js
+++ js/main.js
@@ -4,18 +4,33 @@
 for (let i=1; i <=100; i++){
     if(i  %  3 == 0  && i % 5 == 0){
      console.log('FizzBuzz')
+     document.getElementById("container-for-squares").innerHTML += `
+     <div class="squarefizzbuzz">
+         <p>"Fizzbuzz"</p>
+     </div>
+     `;
    }else if(i  %  3 == 0){
      console.log('Fizz')
+     document.getElementById("container-for-squares").innerHTML += `
+     <div class="squarefizz">
+         <p>"Fizz"</p>
+     </div>
+     `;
    }else if(i % 5 == 0){
      console.log('Buzz')
+     document.getElementById("container-for-squares").innerHTML += `
+     <div class="squarebuzz">
+         <p>"Buzz"</p>
+     </div>
+     `;
    }else{
     console.log(i)
-   }
-   document.getElementById("container-for-squares").innerHTML += `
-   <div class="square">
-       <p>${i}</p>
-   </div>
-   `;
-    
- }
+    document.getElementById("container-for-squares").innerHTML += `
+    <div class="square">
+        <p>${i}</p>
+    </div>
+    `;
+    }
+   }    
+
 
